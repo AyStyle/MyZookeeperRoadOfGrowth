30b986d16726c7e3b7833ab2d57661ecad1d2bfb
逍遥Ay_Style <dreedisgood@qq.com>
2020-12-09 21:51:50 +0800

    添加CuratorFramework单元测试

--- src/test/java/ankang/zookeeper/learn/ZkCuratorDemoTest.java
+++ src/test/java/ankang/zookeeper/learn/ZkCuratorDemoTest.java
@@ -2,11 +2,16 @@ package ankang.zookeeper.learn;
 
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.framework.api.CuratorEvent;
+import org.apache.curator.framework.api.CuratorListener;
+import org.apache.curator.framework.recipes.cache.CuratorCache;
+import org.apache.curator.framework.recipes.cache.CuratorCacheListener;
+import org.apache.curator.framework.recipes.cache.PathChildrenCacheEvent;
+import org.apache.curator.framework.recipes.watch.PersistentWatcher;
 import org.apache.curator.retry.RetryForever;
 import org.apache.zookeeper.CreateMode;
 import org.apache.zookeeper.WatchedEvent;
 import org.apache.zookeeper.Watcher;
-import org.apache.zookeeper.proto.WatcherEvent;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
@@ -14,8 +19,6 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Comparator;
 import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class ZkCuratorDemoTest {
 
@@ -26,8 +29,8 @@ class ZkCuratorDemoTest {
     @BeforeAll
     static void init() throws Exception {
         cf = CuratorFrameworkFactory.newClient("localhost:2181" , new RetryForever(3000));
-        cf.start();
 
+        cf.start();
         cf.create().creatingParentsIfNeeded().withMode(CreateMode.PERSISTENT).inBackground().forPath(TEST_PATH);
     }
 
@@ -43,7 +46,6 @@ class ZkCuratorDemoTest {
     /**
      * 测试获取children节点
      * 结论：返回的信息为节点名称，不包含路径
-     * 收获：watched()方法会设置一个监听，并等待所有的inBackground方法执行完成
      */
     @Test
     void testGetChildren() throws Exception {
@@ -58,8 +60,7 @@ class ZkCuratorDemoTest {
         cf.create().withMode(CreateMode.EPHEMERAL).inBackground().forPath(parentPath + "/" + childNode2);
         cf.create().withMode(CreateMode.EPHEMERAL).inBackground().forPath(parentPath + "/" + childNode3);
 
-        // watched()：会设置一个监听，并等待所有inBackground操作完成
-        final List<String> children = cf.getChildren().watched().forPath(parentPath);
+        final List<String> children = cf.getChildren().forPath(parentPath);
         children.sort(Comparator.naturalOrder());
         for (int i = 0 ; i < children.size() ; i++) {
             final String child = children.get(i);
@@ -67,6 +68,67 @@ class ZkCuratorDemoTest {
         }
 
         cf.delete().deletingChildrenIfNeeded().inBackground().forPath(parentPath);
+    }
+
+    /**
+     * 测试watcher机制
+     * 结论：一次注册N次使用
+     * 收获：
+     * 1. 使用缓存设置监听时，一定要使用 {@link CuratorCache#bridgeBuilder}方法，这个构造方法在节点信息变更时会自动监听
+     * 2. 使用{@link CuratorCache#bridgeBuilder#withDataNotCached}方法构造的缓存无法监听节点删除事件
+     *
+     * @throws Exception
+     */
+    @Test
+    void testCuratorCache() throws Exception {
+        final String parentPath = TEST_PATH + "/cache";
+        final String childNode1 = "child_0";
+        final String childNode2 = "child_1";
+        final String childNode3 = "child_2";
+
+        final CuratorCache cc = CuratorCache.bridgeBuilder(cf , parentPath).build();
+        cc.start();
+
+        cc.listenable().addListener(CuratorCacheListener.builder().forPathChildrenCache(parentPath , cf , (CuratorFramework client , PathChildrenCacheEvent event) -> {
+            switch (event.getType()) {
+                case INITIALIZED:
+                    System.out.println(String.format("init path: %s, data: %s, listData: %s" , event.getData().getPath() , new String(event.getData().getData()) , event.getInitialData().toString()));
+                    break;
+                case CHILD_ADDED:
+                    System.out.println(String.format("add path: %s, data: %s" , event.getData().getPath() , new String(event.getData().getData())));
+                    break;
+                case CHILD_UPDATED:
+                    System.out.println(String.format("update path: %s, data: %s" , event.getData().getPath() , new String(event.getData().getData())));
+                    break;
+                case CHILD_REMOVED:
+                    System.out.println(String.format("remove path: %s, data: %s" , event.getData().getPath() , new String(event.getData().getData())));
+                    break;
+                default:
+                    System.out.println(event.getType());
+                    break;
+            }
+        }).build());
+
+        try {
+            cf.create().withMode(CreateMode.PERSISTENT).inBackground().forPath(parentPath);
+
+            cf.create().inBackground().forPath(parentPath + "/" + childNode1 , childNode1.getBytes());
+            Thread.sleep(1000);
+
+            cf.create().inBackground().forPath(parentPath + "/" + childNode2 , childNode2.getBytes());
+            Thread.sleep(1000);
+
+            cf.create().withMode(CreateMode.EPHEMERAL).inBackground().forPath(parentPath + "/" + childNode3 , childNode3.getBytes());
+            Thread.sleep(1000);
+
+            cf.delete().inBackground().forPath(parentPath + "/" + childNode1);
+            Thread.sleep(1000);
+
+            cf.setData().inBackground().forPath(parentPath + "/" + childNode2 , (childNode2 + childNode2).getBytes());
+            Thread.sleep(1000);
+        } finally {
+            cc.close();
+        }
     }
 
 }
