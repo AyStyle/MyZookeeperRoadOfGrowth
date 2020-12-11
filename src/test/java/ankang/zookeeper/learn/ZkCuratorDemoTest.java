5d030e7e8cd974ec2e2c1531a10bc692091098f4
逍遥Ay_Style <dreedisgood@qq.com>
2020-12-12 02:09:38 +0800

    添加单元测试，测试CuratorCache CreateAndChange

--- src/test/java/ankang/zookeeper/learn/ZkCuratorDemoTest.java
+++ src/test/java/ankang/zookeeper/learn/ZkCuratorDemoTest.java
@@ -2,16 +2,12 @@ package ankang.zookeeper.learn;
 
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
-import org.apache.curator.framework.api.CuratorEvent;
-import org.apache.curator.framework.api.CuratorListener;
+import org.apache.curator.framework.recipes.cache.ChildData;
 import org.apache.curator.framework.recipes.cache.CuratorCache;
 import org.apache.curator.framework.recipes.cache.CuratorCacheListener;
 import org.apache.curator.framework.recipes.cache.PathChildrenCacheEvent;
-import org.apache.curator.framework.recipes.watch.PersistentWatcher;
 import org.apache.curator.retry.RetryForever;
 import org.apache.zookeeper.CreateMode;
-import org.apache.zookeeper.WatchedEvent;
-import org.apache.zookeeper.Watcher;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
@@ -19,12 +15,14 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.concurrent.Executors;
 
 class ZkCuratorDemoTest {
 
     private static String TEST_PATH = "/curator_test";
 
     private static CuratorFramework cf;
+    private static CuratorCache cc;
 
     @BeforeAll
     static void init() throws Exception {
@@ -32,6 +30,9 @@ class ZkCuratorDemoTest {
 
         cf.start();
         cf.create().creatingParentsIfNeeded().withMode(CreateMode.PERSISTENT).inBackground().forPath(TEST_PATH);
+
+        cc = CuratorCache.bridgeBuilder(cf , TEST_PATH).build();
+        cc.start();
     }
 
     @AfterAll
@@ -39,6 +40,7 @@ class ZkCuratorDemoTest {
         try {
             cf.delete().deletingChildrenIfNeeded().forPath(TEST_PATH);
         } finally {
+            cc.close();
             cf.close();
         }
     }
@@ -54,7 +56,7 @@ class ZkCuratorDemoTest {
         final String childNode2 = "child_1";
         final String childNode3 = "child_2";
 
-        cf.create().withMode(CreateMode.PERSISTENT).inBackground().forPath(parentPath);
+        cf.create().creatingParentsIfNeeded().withMode(CreateMode.PERSISTENT).inBackground().forPath(parentPath);
 
         cf.create().withMode(CreateMode.EPHEMERAL).inBackground().forPath(parentPath + "/" + childNode1);
         cf.create().withMode(CreateMode.EPHEMERAL).inBackground().forPath(parentPath + "/" + childNode2);
@@ -85,9 +87,6 @@ class ZkCuratorDemoTest {
         final String childNode1 = "child_0";
         final String childNode2 = "child_1";
         final String childNode3 = "child_2";
-
-        final CuratorCache cc = CuratorCache.bridgeBuilder(cf , parentPath).build();
-        cc.start();
 
         cc.listenable().addListener(CuratorCacheListener.builder().forPathChildrenCache(parentPath , cf , (CuratorFramework client , PathChildrenCacheEvent event) -> {
             switch (event.getType()) {
@@ -129,6 +128,39 @@ class ZkCuratorDemoTest {
         } finally {
             cc.close();
         }
+    }
+
+    /**
+     * 测试CuratorCache的createAndChange缓存
+     * 结论：
+     * 1. 当创建节点时：oldNode为null，newNode不为null
+     * 2. 当更新节点时：oldNode不为null，且数据为更新之前的数据；newNode不为null，数据为更新后的数据
+     * 3. 监听是递归的，如果节点是递归（多层子节点）的，那么监听器也能监听到。
+     */
+    @Test
+    void testCuratorCacheCreateAndChange() throws Exception {
+        final String path = TEST_PATH + "/cacheCreateAndChange/aaa/bbb/ccc/ddd/eee/fff/ggg";
+
+        cc.listenable().addListener(CuratorCacheListener.builder().forCreatesAndChanges((ChildData oldNode , ChildData newNode) -> {
+            System.out.println("============================");
+            if (oldNode != null) {
+                System.out.println("old: " + oldNode.getPath());
+                System.out.println(String.format("old: %s, %s" , oldNode.getPath() , new String(oldNode.getData())));
+            } else {
+                System.out.println("old: null");
+            }
+            if (newNode != null) {
+                System.out.println("new: " + newNode.getPath());
+                System.out.println(String.format("new: %s, %s" , newNode.getPath() , new String(newNode.getData())));
+            } else {
+                System.out.println("new: null");
+            }
+            System.out.println("---------------------------");
+        }).build() , Executors.newSingleThreadExecutor());
+
+        cf.create().creatingParentsIfNeeded().withMode(CreateMode.PERSISTENT).forPath(path , "data1111111".getBytes());
+        cf.setData().forPath(path , "data2222222".getBytes());
+        Thread.sleep(10000);
     }
 
 }
